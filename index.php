d10b582d4575a7703b8779f7e1456ddf3542b07c
Keith Casey <keith@caseysoftware.com>
2014-10-16 15:32:36 -0500

    wiring in the default page

--- index.php
+++ index.php
@@ -14,16 +14,6 @@
 
     <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 
 <body>
@@ -37,23 +27,25 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="index.php">Clarify Video Demo</a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="load.php">Load File</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
 </div>
 
 <div class="container">
-
     <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        <h1></h1>
+        <p class="lead">Submit your search terms:</p>
+        <form action="search.php" method="POST">
+            <input name="terms" value="" />
+            <input type="submit" value="Search!" />
+        </form>
     </div>
 
 </div><!-- /.container -->
