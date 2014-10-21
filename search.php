0a93e28a0bb50c2ac68d338997567f8a0c34f66d
Keith Casey <keith@caseysoftware.com>
2014-10-20 23:14:28 -0500

    merging the search results into the page

--- search.php
+++ search.php
@@ -2,38 +2,39 @@
 include 'creds.php';
 include 'vendor/autoload.php';
 
-$terms = $_GET['terms'];
+$terms = $_POST['terms'];
 $terms = preg_replace("/[^A-Za-z0-9|]/", "", $terms);
 
 $bundle = new Clarify\Bundle($apikey);
 $items = $bundle->search($terms);
-
-$search_terms = json_encode($items['search_terms']);
-$item_results = json_encode($items['item_results']);
-
-$bundlekey = $items['_links']['items'][0]['href'];
-$tracks = $bundle->tracks->load($bundlekey)['tracks'];
-
-$mediaUrl = $tracks[0]['media_url'];
-$duration = $tracks[0]['duration'];
+echo '<pre>'; print_r($items); echo '</pre>';
+$total = (int) $items['total'];
+//$search_terms = json_encode($items['search_terms']);
+//$item_results = json_encode($items['item_results']);
+//
+//$bundlekey = $items['_links']['items'][0]['href'];
+//$tracks = $bundle->tracks->load($bundlekey)['tracks'];
+//
+//$mediaUrl = $tracks[0]['media_url'];
+//$duration = $tracks[0]['duration'];
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=9"/>
-    <title>Clarify Video Player Demo</title>
-    <style type="text/css">
-        body { font-family: sans-serif; }
-    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-    <script src="scripts/jquery/jquery-ui-1.9.2.custom.min.js" type="text/javascript"></script>
-    <script src="scripts/jquery/jquery.jplayer-2.6.0.min.js" type="text/javascript"></script>
-    <script src="scripts/o3v_video_player.js" type="text/javascript"></script>
+    <title>Starter Template for Bootstrap</title>
 
-    <link rel="stylesheet" href="css/jquery-ui.custom.css"/>
-    <link rel="stylesheet" href="css/o3v-player.css"/>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
 
     <script type="text/javascript">
         $(document).ready(function() {
@@ -48,23 +49,18 @@ $duration = $tracks[0]['duration'];
             // Create a player on the page
             o3vPlayer.createPlayer("#player_instance_1",mediaURLs,9);
 
-            o3vPlayer.createPlayer("#player_instance_3",mediaURLs);
-
             ////////////////////////////////////////////////////////
-
             // This is a sample search_terms array from a SearchCollection
             var searchTerms = <?php echo $search_terms; ?>;
-
             // This is a sample "ItemResult" object from a SearchCollection JSON
             // object. It is one item in the item_results array.
             var itemResult =  <?php echo substr($item_results, 1, -1); ?>;
-
             ////////////////////////////////////////////////////////
 
             // Create a player and add in search results marks
 
             var convDuration = <?php echo $duration; ?>;
-            var player = o3vPlayer.createPlayer("#player_instance_2",mediaURLs,
+            var player = o3vPlayer.createPlayer("#player_instance_1",mediaURLs,
                 convDuration,{volume:0.5});
             o3vPlayer.addItemResultMarkers(player,convDuration,itemResult,searchTerms);
 
@@ -77,11 +73,11 @@ $duration = $tracks[0]['duration'];
                 var dtag = document.createElement('div');
                 $(dtag).addClass("o3v-search-tag o3v-search-color-"+i);
                 $(dtag).text(term);
-                $("#player_2_search_tags").append(dtag);
+                $("#player_1_search_tags").append(dtag);
             }
             dtag = document.createElement('div');
             $(dtag).addClass("o3v-clear");
-            $("#player_2_search_tags").append(dtag);
+            $("#player_1_search_tags").append(dtag);
             ////////////////////////////////////////////////////////
 
         });
@@ -89,15 +85,28 @@ $duration = $tracks[0]['duration'];
 
 </head>
 <body>
-<h3>Clarify JPlayer Video Demo</h3>
-<form action="" method="GET">
-    Search terms: <input name="terms" value="" />
-    <input type="submit" />
-</form>
-<br>
-Player Example:
-<br>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Clarify Video Demo</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="load.php">Load File</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+<?php if (0 == $total) { ?>
 <em>If no video player appears, there was not a search result found.</em>
+<?php } ?>
 <br>
 <div id="player_1_search_tags" class="o3v-search-tag-box"></div>
 <div id="player_instance_1"></div>
